4000cc0c082b5b1c5be8390ddff8d4317aa55936
Jost Hren <jost.hren@getinge.com>
2024-12-19 20:52:30 +0100

    feat(fe): import fonts

--- src/themes/AppThemeProvider.tsx
+++ src/themes/AppThemeProvider.tsx
@@ -3,6 +3,15 @@ import { useAppSelector } from '@/app/store';
 import { PaletteMode } from '@mui/material';
 import React from 'react';
 
+import '@fontsource/ubuntu/300.css';
+import '@fontsource/ubuntu/400.css';
+import '@fontsource/ubuntu/500.css';
+import '@fontsource/ubuntu/700.css';
+import '@fontsource/montserrat/300.css';
+import '@fontsource/montserrat/400.css';
+import '@fontsource/montserrat/500.css';
+import '@fontsource/montserrat/700.css';
+
 type Props = {
   children?: React.ReactNode;
 };
